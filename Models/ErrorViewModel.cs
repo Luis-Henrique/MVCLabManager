5e098927f7ef175daa94212af8e280bfcfa45a73
Luis <Henriqueluis3104@hotmail.com>
2022-11-16 10:56:45 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace mvcLabManager.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
